3c4312da3788aab6fb2693454ea2c85c0851cb47
Brain Mentors Pvt Ltd <brainmentorspvtltd@gmail.com>
2022-09-27 10:16:45 +0530

    Add files via upload

---
+++ Section_B/DataTypes.java
@@ -0,0 +1,25 @@
+import java.math.BigInteger;
+
+public class DataTypes {
+    public static void main(String[] args) {
+        byte b = 12;
+        short s = 45;
+        int i = 23343444;
+
+        float f = 45.5f;
+        double d = 56.23;
+
+        char c = 'h';
+        int c1 = 'a';
+
+        String str = "hello";
+
+        Integer in = 44544;
+        int arr[] = {3,5,3,3};
+
+        BigInteger big = new BigInteger("4354353453453543");
+        BigInteger big_2 = new BigInteger("1012");
+        big.add(big_2);
+
+    }
+}
