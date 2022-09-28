b104f20c90adeb73cf1995f7b8f9d552db85786e
Brain Mentors Pvt Ltd <brainmentorspvtltd@gmail.com>
2022-09-28 12:09:23 +0530

    Add files via upload

---
+++ Section_D/BasicCalc.java
@@ -0,0 +1,11 @@
+public class BasicCalc {
+    public static void main(String[] args) {
+        System.out.println("Hello Using Java...");
+        int fnum = Integer.parseInt(args[0]);
+        int snum = Integer.parseInt(args[1]);
+
+        int res = fnum + snum;
+        System.out.println("Result is : " + res);
+
+    }
+}
---
+++ Section_D/Calc.java
@@ -0,0 +1,21 @@
+public class Calc {
+    public static void main(String[] args) {
+        int fnum = Integer.parseInt(args[0]);
+        int snum = Integer.parseInt(args[2]);
+        String opr = args[1];
+        int res = 0;
+        if(opr.equals("+")) {
+            res = fnum + snum;
+        }
+        else if(opr.equals("-")) {
+            res = fnum - snum;
+        }
+        else if(opr.equals("/")) {
+            res = fnum / snum;
+        }
+        else if(opr.equals("x")) {
+            res = fnum * snum;
+        }
+        System.out.println("Result is :: " + res);
+    }
+}
