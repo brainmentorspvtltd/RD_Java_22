1f74e60e2e891c6a8639c4b6ac3b28f6b4cce6bc
Brain Mentors Pvt Ltd <brainmentorspvtltd@gmail.com>
2023-03-11 10:42:40 +0530

    Add files via upload

---
+++ Section_B/Arrays1D/BinarySearch.java
@@ -0,0 +1,24 @@
+package Arrays1D;
+
+public class BinarySearch {
+    public static void main(String[] args) {
+        int arr[] = {10,20,30,40,50,60,70,80,90};
+        int search = 70;
+        int low = 0;
+        int high = arr.length - 1;
+        int mid = 0;
+        while(low < high) {
+            mid = (low + high) / 2;
+            if(search == arr[mid]) {
+                System.out.println("Element Found...");
+                return;
+            }
+            else if(search > arr[mid]) {
+                low = mid + 1;
+            }
+            else if(search < arr[mid]) {
+                high = mid - 1;
+            }
+        }
+    }
+}
---
+++ Section_B/Arrays1D/GasStation.java
@@ -0,0 +1,7 @@
+package Arrays1D;
+
+public class GasStation {
+    public static void main(String[] args) {
+        
+    }
+}
