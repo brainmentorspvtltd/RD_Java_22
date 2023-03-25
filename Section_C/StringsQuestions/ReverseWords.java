d83949a3e715ac01fa1b362ac99814ba0c6d1c00
Brain Mentors Pvt Ltd <brainmentorspvtltd@gmail.com>
2023-03-25 12:35:47 +0530

    Add files via upload

--- Section_C/StringsQuestions/ReverseWords.java
+++ Section_C/StringsQuestions/ReverseWords.java
@@ -1,16 +1,21 @@
-package StringsQuestions;
+package ArraysStrings;
 
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 public class ReverseWords {
+
+    static void reverse(String str) {
+        str = str.trim();   // remove leading and trailing spaces
+        String[] words = str.split(" ");    // {"hello", "world"}
+        List<String> wordList = Arrays.asList(words);   // ["hello", "world"]
+        Collections.reverse(wordList);  // {"world", "hello"}
+        String result = String.join(" ", wordList);     // "world hello"
+        System.out.println(result);
+    }
     public static void main(String[] args) {
-        String str = "the sky is blue";
-        str = str.trim(); // remove leading and trailing spaces
-        List<String> wordList = Arrays.asList(str.split(" "));
-        Collections.reverse(wordList);
-        String.join(" ", wordList);
-        
+        String str = "hello world";
+        reverse(str);
     }
 }
