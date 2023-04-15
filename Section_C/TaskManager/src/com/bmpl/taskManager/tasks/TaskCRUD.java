4a9d5f1a7ff83a6c298f28f84376df50f10a7b0d
Brain Mentors Pvt Ltd <brainmentorspvtltd@gmail.com>
2023-04-15 12:35:53 +0530

    Add files via upload

--- Section_C/TaskManager/src/com/bmpl/taskManager/tasks/TaskCRUD.java
+++ Section_C/TaskManager/src/com/bmpl/taskManager/tasks/TaskCRUD.java
@@ -4,8 +4,14 @@ import java.util.ArrayList;
 
 public class TaskCRUD {
 	ArrayList<Task> taskList;
-	public void addTask() {
-		
+	public TaskCRUD() {
+		taskList = new ArrayList<>();
+	}
+	public void addTask(String taskName, String taskDesc) {
+		Task task = new Task();
+		task.setTaskName(taskName);
+		task.setTaskDesc(taskDesc);
+		taskList.add(task);
 	}
 	public void deleteTask() {
 		
--- Section_C/TaskManager/src/com/bmpl/taskManager/views/View.java
+++ Section_C/TaskManager/src/com/bmpl/taskManager/views/View.java
@@ -2,12 +2,15 @@ package com.bmpl.taskManager.views;
 
 import java.util.Scanner;
 
+import com.bmpl.taskManager.tasks.TaskCRUD;
+
 public class View {
 
 	public static void main(String[] args) {
 		
 		Scanner scanner = new Scanner(System.in);
-		
+		// Add messages.properties file and 
+		// read all messages from that file
 		System.out.println("1. Enter new task : ");
 		System.out.println("2. Print all task : ");
 		System.out.println("3. Delete task : ");
@@ -23,7 +26,8 @@ public class View {
 			String taskDesc = scanner.nextLine();
 			// Make object of TaskCRUD
 			// call addTask and pass taskName and desc
-
+			TaskCRUD crud = new TaskCRUD();
+			crud.addTask(taskName, taskDesc);
 		}
 		default:
 			
